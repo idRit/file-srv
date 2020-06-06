3a99c90e0f57933f5d72d2d57eb6c4c3e847275b
Ritwik Mukherjee <mritwik369@gmail.com>
2020-06-06 18:31:15 +0530

    removed ftp passsword

--- ftp.client.js
+++ ftp.client.js
@@ -11,7 +11,7 @@ const downloadBuffer = __dirname + "/dl/";
 const config = {
     host: "127.0.0.1",
     user: "ritwikm",
-    password: "blxgre3",
+    password: "ftp_pass",
     secure: false
 }
 
